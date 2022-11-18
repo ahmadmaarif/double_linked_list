7a663f0b4f737fbb7bad4fcfe81ff2358fde7215
Ahmad Maarif <ahmad.maarif.ft21@mail.umy.ac.id>
2022-11-18 17:12:32 +0700

    create constructor

--- double_linked_list/Program.cs
+++ double_linked_list/Program.cs
@@ -6,19 +6,48 @@ using System.Threading.Tasks;
 
 namespace double_linked_list
 {
-    class Node 
+   
     }
-        /*Node class represent the node of doubly linked list
-         * its consict of the information part and and list to
-         * its seccuding and preceeding 
-         * in terms of next and previous */
-        public int noMhs;
-        public string name;
-        //point to the succeding node 
-        public Node next;
-        //point the preceending node
-        public Node prev;
-{       
+        class Node 
+        {
+            /*Node class represent the node of doubly linked list
+            * its consict of the information part and and list to
+            * its seccuding and preceeding 
+            * in terms of next and previous */
+            public int noMhs;
+            public string name;
+            //point to the succeding node 
+            public Node next;
+            //point the preceending node
+            public Node prev;
+        class DoubleLinkedList
+        {
+            Node STAR;
+
+            //constructor
+
+            public void addNode()
+            {
+            int nim;
+            string nm;
+            Console.WriteLine("\nEnter the roll number of the student:");
+            nim = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\nEnter the name of the student:");
+            nm = Console.ReadLine();
+            Node newNode = new Node();
+            newNode.noMhs = nim;   
+            newNode.name = nm;
+
+            //check if the list empty
+            if(STAR == null || nim<=STAR.noMhs)
+            {
+                if((STAR!=null) && (nim == STAR.noMhs))
+                {
+
+                }
+            }
+            }
+        }
     internal class Program
     {
         static void Main(string[] args)
