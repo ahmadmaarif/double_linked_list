acca118f5c9852ff2543102ee68ae76aeb1b7904
Ahmad Maarif <ahmad.maarif.ft21@mail.umy.ac.id>
2022-11-18 16:53:49 +0700

    Add project files.

---
+++ double_linked_list/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace double_linked_list
+{
+    class Node 
+    }
+        /*Node class represent the node of doubly linked list
+         * its consict of the information part and and list to
+         * its seccuding and preceeding 
+         * in terms of next and previous */
+        public int noMhs;
+        public string name;
+        //point to the succeding node 
+        public Node next;
+        //point the preceending node
+        public Node prev;
+{       
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
